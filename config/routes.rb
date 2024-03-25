21e7b414845d25f837c824793cd5ef87f81dc183
Andrew Pham <minhanh.vietpham@gmail.com>
2024-03-25 17:06:55 -0500

    connected database to protgresql and manually set up rails:db. Also created devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  mount RailsDb::Engine => '/rails/db', :as => 'rails_db'
+  root to: "boards#index"
+  
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "boards#index"
 end
