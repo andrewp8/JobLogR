0a9e7b842f1d674812de4a2d1123ae34f103aa60
Andrew Pham <minhanh.vietpham@gmail.com>
2024-03-27 10:31:16 -0500

    added shared partials, and work-for-now nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :interviews
   root to: "boards#index"
   # mount RailsDb::Engine => '/rails/db', :as => 'rails_db'
   
   devise_for :users
   resources :job_listings
   resources :boards
+  resources :interviews
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
