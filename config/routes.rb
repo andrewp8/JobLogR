3d347bbab55bbef575d5a44670ad124de1437a6b
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-03-29 12:27:10 +0000

    css-ed log-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root to: "pages#landing"
-  # mount RailsDb::Engine => '/rails/db', :as => 'rails_db'
   
-  devise_for :users
   resources :job_listings
   resources :boards
   resources :interviews
   
+  devise_for :users
+  # mount RailsDb::Engine => '/rails/db', :as => 'rails_db'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
