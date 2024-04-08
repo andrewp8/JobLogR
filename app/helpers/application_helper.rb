24c66a2046d2943865901d02b3dd82bb85fd5259
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-04-08 18:01:38 +0000

    markdown worked

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def markdown_to_html(markdown_content)
+      renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+      markdown = Redcarpet::Markdown.new(renderer, extensions = {autolink: true, no_intra_emphasis: true})
+      markdown.render(markdown_content).html_safe
+      
+  end
 end
