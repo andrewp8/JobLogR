3de6ac6abdf51e780f713b73f49cca5c1c5b0926
Andrew Pham <minhanh.vietpham@gmail.com>
2024-04-12 08:13:31 -0500

    Define mailer methods

--- app/mailers/follow_up_mailer.rb
+++ app/mailers/follow_up_mailer.rb
@@ -1,2 +1,11 @@
 class FollowUpMailer < ApplicationMailer
+  def follow_up_email(recipient_email, follow_up_template, email_body, current_user, job_listing)
+    @recipient_email = recipient_email
+    @follow_up_template = follow_up_template
+    @body = email_body
+    @current_user = current_user
+    @job_listing = job_listing
+
+    mail(to: recipient_email, subject: "Follow Up: #{current_user.first_name.capitalize} #{current_user.last_name.capitalize} - #{@job_listing.title}")
+  end
 end
