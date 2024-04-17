bdb4bc0eeab7994e93c7bad533bb41d6d46d7ca6
Andrew Pham <minhanh.vietpham@gmail.com>
2024-04-17 12:29:17 -0500

    re-factored Ai_messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,9 @@ module ApplicationHelper
       renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
       markdown = Redcarpet::Markdown.new(renderer, extensions = {autolink: true, no_intra_emphasis: true})
       markdown.render(markdown_content).html_safe
-      
+  end
+
+  def set_flash_error(object)
+    flash[:error] = object.errors.full_messages.to_sentence if object.errors.any?
   end
 end
--- app/models/ai_message.rb
+++ app/models/ai_message.rb
@@ -19,4 +19,17 @@
 #
 class AiMessage < ApplicationRecord
   belongs_to :job_listing, foreign_key: :job_listing_id
+
+  def self.generate_response
+    client = OpenAI::Client.new(access_token: ENV["OPENAI_API_KEY"])
+    instructed_prompt = "As a career expert, please answer my question #{self.body[0]}. Give me a specific advices, insights, recommendations, url if necessary. If i ever ask about resume or cover letter, please provide important key to add in my resume or cover letter for the company if applicable. Please respond in Markdown syntax language. Please feel free to refuse answer questions which doesn't have anything to do with job search."
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: instructed_prompt }],
+        temperature: 0.7,
+      },
+    )
+    return response["choices"][0]["message"]["content"]
+  end
 end
