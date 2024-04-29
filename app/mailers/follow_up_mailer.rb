f5ade6c9f78e80453e629029adfef7a9624427e9
Andrew Pham <minhanh.vietpham@gmail.com>
2024-04-29 14:17:48 -0500

    install postmark-rails, postmark, and json. Agreed Xcode license agreements

--- app/mailers/follow_up_mailer.rb
+++ app/mailers/follow_up_mailer.rb
@@ -1,8 +1,6 @@
 class FollowUpMailer < ApplicationMailer
   def follow_up_email(recipient_email, email_body, subject)
 
-    # TODO: this from will not work as intended
-    # Andrew Pham <notifications@joblogr.com>
     mail(from: ENV["GMAIL_ADDRESS"], to: recipient_email, subject:  subject) do |format|
       format.html { render html: email_body.html_safe }
     end
